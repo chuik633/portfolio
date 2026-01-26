c814ef49b9420042b32d879e4f96ecc293dcc3e4
Katherine Chui <chuik633@newschool.edu>
2026-01-26 11:34:39 -0800

    deploy debug

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const base = process.env.VITE_BASE || "/"; // use env for GH Pages, default root for Render
+
 export default defineConfig({
-  base: "/portfolio/",
+  base,
 
   plugins: [vue()],
 
